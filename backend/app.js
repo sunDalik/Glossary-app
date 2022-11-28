331a1327749ad05aa90eb49bea09dfafcee6cacc
sunDalik <dali1999@mail.ru>
2022-11-28 21:23:53 +0300

    Add glossary backend skeleton

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,43 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-const cors = require('cors')
+const cors = require('cors');
 
 const server = app.listen(port, () => {
     console.log(`Node.js server is listening at port ${port}`);
 });
 
-app.use(cors())
+//app.use(cors())
+
+const dictionary = {};
 
 app.get('/', (req, res) => {
     res.send('sundalik');
 });
 
+function isValidQueryValue(value) {
+    return value !== undefined && value !== null && value !== "";
+}
 
-app.get('/login/', (req, res) => {
+// TODO change to post
+// /add?key=persona&value=awake
+app.get('/add', (req, res) => {
+    const key = req.query.key;
+    const value = req.query.value;
+    if (!isValidQueryValue(key) || !isValidQueryValue(value)) {
+        res.sendStatus(400);
+        return;
+    }
+    dictionary[key] = value;
+    res.sendStatus(200);
+});
+
+app.get('/read', (req, res) => {
+    const searchString = isValidQueryValue(req.query.search) ? req.query.search : '';
+    const limit = isValidQueryValue(req.query.limit) ? req.query.limit : 1000;
+    res.send(JSON.stringify(dictionary));
+});
+
+app.get('/author/', (req, res) => {
     res.send('sundalik');
 });
