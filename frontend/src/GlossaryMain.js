0349519283ffb6b785b371c9abefc5774fc6fd99
sunDalik <dali1999@mail.ru>
2022-11-29 20:04:22 +0300

    Display local dictionary

--- frontend/src/GlossaryMain.js
+++ frontend/src/GlossaryMain.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import './GlossaryMain.css'
 
 function GlossaryMain() {
+  const [dictionary, setDictionary] = useState([{ key: 'lol', value: 'test' }, { key: 'persona', value: 'ravage them' },]);
+
   return (
-    <div>
-      Glossary Main content
+    <div className='GM_page'>
+      <div className='GM_header'>Glossary</div>
+
+      <ul>
+        {dictionary.map((entry) => {
+          return (<div key={entry.key} className='GM_dictContainer'>
+            <div className='GM_dictKey'>{entry.key}</div>
+            <div className='GM_dictValue'>{entry.value}</div>
+          </div>)
+        })}
+      </ul>
     </div>
   );
 }
--- frontend/src/NotFound.js
+++ frontend/src/NotFound.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
-    <div className='page'>
+    <div className='NotFound_page'>
       <div> Oops, this page does not exist </div>
       <Link to="/">Go to main page</Link>
     </div>
