225131855ac7ff680f303b6c9ead113c88b15813
sunDalik <dali1999@mail.ru>
2022-11-29 20:13:04 +0300

    Add new entry form

--- frontend/src/GlossaryMain.js
+++ frontend/src/GlossaryMain.js
@@ -1,13 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './GlossaryMain.css'
 
 function GlossaryMain() {
   const [dictionary, setDictionary] = useState([{ key: 'lol', value: 'test' }, { key: 'persona', value: 'ravage them' },]);
+  const [newKey, setNewKey] = useState('');
+  const [newValue, setNewValue] = useState('');
+
+
+  useEffect(() => {
+    //TODO fetch
+  }, []);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setDictionary([{ key: newKey, value: newValue }, ...dictionary]);
+    //TODO post
+  };
 
   return (
     <div className='GM_page'>
       <div className='GM_header'>Glossary</div>
-
+      <form onSubmit={onSubmit}>
+        <input required value={newKey} onChange={e => setNewKey(e.target.value)} placeholder="Key" />
+        <input required value={newValue} onChange={e => setNewValue(e.target.value)} placeholder="Value" />
+        <button type="Submit">Add entry</button>
+      </form>
       <ul>
         {dictionary.map((entry) => {
           return (<div key={entry.key} className='GM_dictContainer'>
